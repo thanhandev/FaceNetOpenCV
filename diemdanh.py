386a6d2a2b548329c1e12ad5de11f29ee8237717
Thành An <83403597+thanhandev@users.noreply.github.com>
2023-01-16 10:03:34 +0700

    Update diemdanh.py

--- diemdanh.py
+++ diemdanh.py
@@ -74,7 +74,7 @@ while True:
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
     
-    cv2.imshow('KHOA CUA THONG MINH',img) 
+    cv2.imshow('DIEM DANH FACEID',img) 
 
     k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
     if k == 27:
